643552ec5ec00a74419290174c1b176f5b79dd69
SimonMarchildon <76969585+SimonMarchildon@users.noreply.github.com>
2021-01-14 17:11:48 -0500

    Update HelloWorld.java

--- ProjetM/src/main/java/edu/ics4u/ProjetM/HelloWorld.java
+++ ProjetM/src/main/java/edu/ics4u/ProjetM/HelloWorld.java
@@ -3,7 +3,7 @@ package edu.ics4u.ProjetM;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("M.Crowley est beau");
         System.exit(0);
     }
 
